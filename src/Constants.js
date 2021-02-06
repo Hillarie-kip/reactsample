034716d35b2e990e4b8e047f8450e4b50e532def
Hillarie-kip <kalyahillary@gmail.com>
2021-02-06 14:26:31 +0300

    https://github.com/Hillarie-kip/reactsample.git

---
+++ src/Constants.js
@@ -0,0 +1,6 @@
+
+export const baseurl = "https://homeapi.esquekenya.com/api/"
+export const getMyHomeProperty = baseurl+'Property/GetProperty';
+
+export const  API_KEY = 'Funguo';
+export const  Funguo = 'LaHNcG1xClzAqZpegrr83gbxmSkZsniZ';
